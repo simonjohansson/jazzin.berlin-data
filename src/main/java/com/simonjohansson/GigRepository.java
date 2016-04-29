834a491df6109cc93b917ff61c2970c162363345
Simon Johansson <simon@simonjohansson.com>
2016-04-29 10:35:47 +0200

    Allow findByDate to return multiple gigs, and add findByDateBetween that can take a range

--- src/main/java/com/simonjohansson/GigRepository.java
+++ src/main/java/com/simonjohansson/GigRepository.java
@@ -1,13 +1,17 @@
 package com.simonjohansson;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.sql.Date;
+import java.util.List;
 
 /**
  * Created by simon on 28/04/16.
  */
 public interface GigRepository extends PagingAndSortingRepository<Gig, Long> {
-    Gig findByDate(@Param("date") Date date);
+    List<Gig> findByDate(@Param("date") Date date);
+    List<Gig> findByDateBetween(@Param("start") Date start, @Param("end") Date end);
 }
+
